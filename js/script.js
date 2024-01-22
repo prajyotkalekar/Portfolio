6f4de3c2c9cd9dd3253f15a6b48e9849755ba467
prajyot <prkalekar1809@gmail.com>
2024-01-22 18:52:36 +0530

    New Changes

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Typing Animation
+
+var typed = new Typed(".typing",{
+    strings:["Computer Engineer","Front-End Developer","SQL Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
