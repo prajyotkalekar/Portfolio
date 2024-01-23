df2eab1e593913a959d120ba4b77b91501c271d0
Prajyot Kalekar <prkalekar1809@gmail.com>
2024-01-23 23:54:43 +0530

    new changes

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Typing Animation
+
+const typed = new Typed(".typing", {
+    strings:["Computer Engineer...","Front-End Developer...","SQL Developer..."],
+    typeSpeed: 30,
+    backSpeed: 30,
+    backDelay: 500,
+    loop: true
+});
