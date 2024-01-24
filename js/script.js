b25f879c30cbcbd0a78325a1237322bb824ce207
Prajyot Kalekar <prkalekar1809@gmail.com>
2024-01-25 01:21:57 +0530

    Project Section Start

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 
 const typed = new Typed(".typing", {
     strings:["Computer Engineer...","Front-End Developer...","SQL Developer..."],
-    typeSpeed: 30,
-    backSpeed: 30,
+    typeSpeed: 50,
+    backSpeed: 50,
     backDelay: 500,
     loop: true
 });
