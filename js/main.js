009872ce768dc2960b88bb0119d468350f185cf8
prajyot <prkalekar1809@gmail.com>
2024-01-25 18:34:28 +0530

    added

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 // Download CV
 
-function  downloadFile() 
-{
-    // name of the file
-    const fileName = "RESUME.pdf";
+// function  downloadFile() 
+// {
+//     // name of the file
+//     const fileName = "RESUME.pdf";
 
-    // path of the file
-    // const filePath = "../Portfolio/CV/"; //relative path
-    const filePath = "C:\\projects\\Portfolio\\CV\\"; //absolute path 
+//     // path of the file
+//      const filePath = "../Portfolio/CV/"; //relative path
+//     //const filePath = "C:\\projects\\Portfolio\\CV\\"; //absolute path 
 
-    const a = document.createElement('a');
-    a.href = filePath + fileName;
-    a.download = fileName;
+//     const a = document.createElement('a');
+//     a.href = filePath + fileName;
+//     //a.download = fileName;
 
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-}
+//     document.body.appendChild(a);
+//     a.click();
+//     document.body.removeChild(a);
+// }
 
 
